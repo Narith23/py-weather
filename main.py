54b6ae9fbffaa962f7f8add7d2026bf18f86a02c
dinarith.duch <dinarith.duch@cogetel.com.kh>
2024-09-13 16:23:33 +0700

    Check Update

--- main.py
+++ main.py
@@ -10,23 +10,12 @@ async def getweather():
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
         # fetch a weather forecast from a city
         temperature_f = await client.get('Phnom Penh')
-
-        # print(temperature_f.temperature)
-
-        # returns the current day's forecast temperature (int)
+        
         # Convert Fahrenheit to Celsius
         temperature_c = (int(temperature_f.temperature) - 32) * 5/9
 
         # Display weather information
         print(f"Temperature: {temperature_c:.2f}°C")
-
-        # # get the weather forecast for a few days
-        # for daily in weather.daily_forecasts:
-        #     print(daily)
-        #
-        #     # hourly forecasts
-        #     for hourly in daily.hourly_forecasts:
-        #         print(f' --> {hourly!r}')
 
 
 if __name__ == '__main__':
